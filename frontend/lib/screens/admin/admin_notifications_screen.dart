c2701084a5748327fbb6ac6eedcdefaa7aa69a83
Mumtaz Ali <ali.mumtaz@virevo.it>
2026-06-10 16:56:28 +0500

    fix: update admin notifications screen

--- frontend/lib/screens/admin/admin_notifications_screen.dart
+++ frontend/lib/screens/admin/admin_notifications_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import '../shared/welcome.dart'; // optional, for back button
+// optional, for back button
 
 class AdminNotificationsScreen extends StatefulWidget {
   final String adminType;
